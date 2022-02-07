c3eb48aee6c1b9c0839a05269d9c37b6fccb2658
Povilas Balzaravičius <povilas@balzaravicius.lt>
2022-02-07 16:57:46 +0200

    Enable mouse keys explicitly

--- keyboards/splitkb/kyria/keymaps/pawka/rules.mk
+++ keyboards/splitkb/kyria/keymaps/pawka/rules.mk
@@ -4,3 +4,4 @@ ENCODER_ENABLE = yes      # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
