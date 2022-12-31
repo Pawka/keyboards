f179c54be878095abf781b4e58cac5ae83372d85
Povilas Balzaravičius <povilas@balzaravicius.lt>
2022-12-31 16:52:39 +0200

    Adjust home-Shift delay

--- keyboards/splitkb/kyria/keymaps/pawka/keymap.c
+++ keyboards/splitkb/kyria/keymaps/pawka/keymap.c
@@ -355,7 +355,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         // Shift
         case HOME_F:
         case HOME_J:
-            return TAPPING_TERM - 50;
+            return TAPPING_TERM - 70;
         case LT_BSPC:
         case LT_ENT:
             return TAPPING_TERM - 70;
